133e059781d6706a72470ec15eec298419617f6b
Roty_ <frency004@gmail.com>
2023-04-13 09:05:34 +0200

    sasso carta forbice

---
+++ TPISIT/sasso_carta_forbice/lib/constant/emoji.dart
@@ -0,0 +1,1 @@
+List<String> emoji = ['✊', '✋', '✌️'];
--- TPISIT/sasso_carta_forbice/lib/main.dart
+++ TPISIT/sasso_carta_forbice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constant/emoji.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -46,9 +32,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var sceltaPL = emoji[0];
+  var sceltaAI = emoji[1];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
           SizedBox(
@@ -62,7 +51,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: TextStyle(color: Colors.cyanAccent, fontSize: 30),
               ),
               SizedBox(
-                width: 120,
+                width: 50,
+              ),
+              Text(
+                'VS',
+                style: TextStyle(color: Colors.black),
+              ),
+              SizedBox(
+                width: 50,
               ),
               Text('AI',
                   style: TextStyle(color: Colors.redAccent, fontSize: 30)),
@@ -73,7 +69,20 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Row(
             children: [
-              
+              SizedBox(
+                width: 110,
+              ),
+              Text(
+                sceltaPL,
+                style: TextStyle(fontSize: 60),
+              ),
+              SizedBox(
+                width: 70,
+              ),
+              Text(
+                sceltaAI,
+                style: TextStyle(fontSize: 60),
+              ),
             ],
           )
         ],
